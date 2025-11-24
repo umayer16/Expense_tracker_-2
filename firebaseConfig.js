540bed40046c80bfa6ca4b22ace607c34bcff4bd
umayer16 <muktadirumayer30@gmail.com>
2025-11-24 20:37:47 +0600

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+// ←←← CREATE THIS FILE EXACTLY ←←←
+const firebaseConfig = {
+  apiKey: "AIzaSyD8fY8sKz1jX9vL5mPqW2nR7tYcV4bN0pQ",
+  authDomain: "aurum-cloud-expenses.firebaseapp.com",
+  projectId: "aurum-cloud-expenses",
+  storageBucket: "aurum-cloud-expenses.appspot.com",
+  messagingSenderId: "487654321987",
+  appId: "1:487654321987:web:a1b2c3d4e5f6g7h8i9j0"
+};
