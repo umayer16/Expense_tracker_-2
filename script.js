b3c5eec8db38bc0c3f1dbbd9b3477cdc594833ed
umayer16 <muktadirumayer30@gmail.com>
2025-11-24 22:04:44 +0600

    Update script.js

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ import { getFirestore, collection, addDoc, onSnapshot, query, orderBy, deleteDoc
 
 // Your Official Firebase Config (Aurum Web – aurum-cloud)
 const firebaseConfig = {
-  apiKey: "AIzaSyBX4W9VMWulR_SkawcPsSs0eTC7igH09ms",
-  authDomain: "aurum-cloud.firebaseapp.com",
-  projectId: "aurum-cloud",
-  storageBucket: "aurum-cloud.firebasestorage.app",
-  messagingSenderId: "999926729828",
-  appId: "1:999926729828:web:636c14afe371f2b526e371",
-  measurementId: "G-TFG7TW6YJT"
+  apiKey: "AIzaSyAXLm688fKgsPWeGYuons5BewqQZcQN2_Q",
+  authDomain: "aurum-ultimate.firebaseapp.com",
+  projectId: "aurum-ultimate",
+  storageBucket: "aurum-ultimate.firebasestorage.app",
+  messagingSenderId: "269240300254",
+  appId: "1:269240300254:web:ff61f1357901cc3b9bc84f",
+  measurementId: "G-F38XYPP0ZT"
 };
 
 const app = initializeApp(firebaseConfig);
